5df358691832a0ee3802ef1b34428bf0b8768052
Alex Ryan <ialexryan@users.noreply.github.com>
2015-02-14 17:25:02 -0800

    Update threedots.js

--- threedots.js
+++ threedots.js
@@ -1,8 +1,7 @@
 document.body.style.background="#ECECEC";
 
-var css = "body{border-top-width: 0px !important;} #header{border-top-width: 0px !important;} #add_from_computer,#add_from_gdrive,#add_from_box{display: none;} /*#google_auth_row{display: none;} #google_auth_separator{display:none;}*/ /*#logo{padding-left: 64px;}*/ ";
-
 cssSetup = function() {
+	var css = "body{border-top-width: 0px !important;} #header{border-top-width: 0px !important;} #add_from_computer,#add_from_gdrive,#add_from_box{display: none;} /*#google_auth_row{display: none;} #google_auth_separator{display:none;}*/ /*#logo{padding-left: 64px;}*/ ";
 	head = document.head || document.getElementsByTagName( 'head' )[ 0 ];
 	style = document.createElement( 'style' );
 	style.type = 'text/css';
